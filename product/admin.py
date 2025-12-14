9ab9c537c2c845731887fd2b528619f6e2a9883e
Izzatillo <izzatillaasatullayev55@gmail.com>
2025-12-14 21:40:42 +0500

    news

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from .models import Cart,Category,Product,Order
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id',  'name', 'created_at']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'payment_type', 'address','total_price']
+    search_fields = ['address']
+    list_filter = ['user']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price','stock','created_at']
+    search_fields = ['name','price','image']
+    list_filter = ['name']
+
+
 admin.site.register(Cart)
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Order,OrderAdmin)
